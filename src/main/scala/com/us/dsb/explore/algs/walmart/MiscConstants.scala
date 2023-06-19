ff36a71d681cbdf16c0e3c649817022a361e2d59
Daniel Barclay <daniel.barclay@qomplx.com>
2023-06-18 22:47:12 -0400

    Cloning color-lines-ai from expl-scala:  Removed most non-com.us.dsb.explore.algs.coloredlines.

--- src/main/scala/com/us/dsb/explore/algs/walmart/MiscConstants.scala
+++
@@ -1,7 +0,0 @@
-package com.us.dsb.explore.algs.walmart
-
-object MiscConstants {
-  val SPEED_UNITS_PER_SEC: Double = DroneParameters.SPEED_UNITS_PER_MIN / 60
-
-  val SECS_PER_DAY: Int = 24 * 60 * 60
-}
