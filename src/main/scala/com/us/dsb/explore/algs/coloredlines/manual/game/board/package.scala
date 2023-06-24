30af3f166bf9e819382a51e64bf8c58435512862
Daniel Barclay <daniel.barclay@qomplx.com>
2023-06-23 22:12:08 -0400

    Deleted unused ColorOrder.

--- src/main/scala/com/us/dsb/explore/algs/coloredlines/manual/game/board/package.scala
+++ src/main/scala/com/us/dsb/explore/algs/coloredlines/manual/game/board/package.scala
@@ -9,8 +9,3 @@ package com.us.dsb.explore.algs.coloredlines.manual.game.board
 /** Order (length) of lines. */
 private type LineOrder = 5
 private[game] val LineOrder: LineOrder = valueOf[LineOrder]
-
-// ???? TODO:  Use this (tie to color enumeration) or remove.
-private type ColorOrder = 7 // original: blue.dark, blue.light, brown, green, purple, red, yellow
-private val ColorOrder: ColorOrder = valueOf[ColorOrder]
-
