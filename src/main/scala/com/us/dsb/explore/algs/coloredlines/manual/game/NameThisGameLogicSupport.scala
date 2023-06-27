3e04ac186394aed7ded44e406982595c7a07fd50
Daniel Barclay <daniel.barclay@qomplx.com>
2023-06-26 22:19:33 -0400

    Optimized pathExists to reduce allocations; enhanced comments.

--- src/main/scala/com/us/dsb/explore/algs/coloredlines/manual/game/NameThisGameLogicSupport.scala
+++ src/main/scala/com/us/dsb/explore/algs/coloredlines/manual/game/NameThisGameLogicSupport.scala
@@ -10,66 +10,79 @@ import scala.collection.mutable
 
 object NameThisGameLogicSupport {
 
+  // Note:  Pulled out of pathExists's looping method to reduce allocations:
+  private val neighborOffsets = List((+1, 0), (-1, 0), (0, +1), (0, -1))
 
-  //???: likely move core algorithm out; possibly move outer code into LowerGameState/Board:
   /**
-   * @param toTapCell - must be empty */
-  // (was "private" before test calls:)
+   * Reports whether ball-movement path exists from given starting cell to
+   * given target cell.
+   * @param board
+   * @param fromBallCell
+   *   address of starting cell (expected to contain ball (requirement undefined))
+   * @param toEmptyCell
+   *   address of target cell (expected to be different from starting cell
+   *   (requirement undefined); must be empty, at least if not starting cell)
+   * @return
+   */
   private[game] def pathExists(board: BoardReadView,
-                               fromBallCell: CellAddress,
-                               toEmptyCell: CellAddress): Boolean = {
-    //??println(s"pathExists: fromBallCell = $fromBallCell, toTapCell = $toTapCell")
-    //???? CLEAN ALL THIS:
-    // - mutability (many eliminate, maybe keep for eventually wanted optimization)
-    // - looping/recursion and exiting (what is class/method that ~loops until flag return value?_
+                              fromBallCell: CellAddress,
+                              toEmptyCell: CellAddress): Boolean = {
+   // ???? TODO:  Revisit looping/recursion.  (What is class/method that
+   //  logically loops until computation-and-control expression returns
+   //  termination value (None?)?  How much instance allocation would that do?)
 
-    // Blocked from (further) "reaching"--by ball or already reached in search.
-    val blockedAt: Array[Array[Boolean]] =
+   // Note:  Using mutable Array and mutable.Queue to reduce instance allocation.
+
+   /** Whether each cell is blocked from (further) "reaching"--by ball or
+    *  already reached in search. */
+    val blockedAt: IndexedSeq[Array[Boolean]] =
       RowIndex.values.map { row =>
           ColumnIndex.values.map { column =>
             board.hasABallAt(CellAddress(row, column))
           }.toArray
-      }.toArray
+      }
     val cellsToExpandFrom = mutable.Queue[CellAddress](fromBallCell)
 
     @tailrec
-    def loop: Boolean = {
-      cellsToExpandFrom.dequeueFirst(_ => true) match {
-        case None =>
-          // no more steps/cells to try
-          false
-        case Some(reachedAddr) =>
-          if (reachedAddr == toEmptyCell) {
-            // there is a path
-            true
-          }
-          else {
-            // no path yet; queue up neighbors neither blocked nor already processed
-            val neighborOffsets = List((+1, 0), (-1, 0), (0, +1), (0, -1))
-            neighborOffsets.foreach { (rowInc, colInc) =>
-              val rowOffset: Int = reachedAddr.row.raw.value    - Index.Origin + rowInc
-              val colOffset: Int = reachedAddr.column.raw.value - Index.Origin + colInc
-              // ???? TODO:  Use Index.MinValue/.MaxValue, etc.?
-              if (! (   0 <= rowOffset && rowOffset < BoardOrder
-                     && 0 <= colOffset && colOffset < BoardOrder)) {
-                // off board
-              } else if (blockedAt(rowOffset)(colOffset)) {
-                // blocked or traversed
-              }
-              else {
-                // open; note traversed and try neighboring cells:
-                blockedAt(rowOffset).update(colOffset, true)
-                val neighborAddress = CellAddress(RowIndex.values(rowOffset),
-                                                  ColumnIndex.values(colOffset))
-                cellsToExpandFrom.enqueue(neighborAddress)
-              }
+    def loopOnNextQueuedAddress: Boolean = {
+      // Note:  Using .isEmpty and .dequeue() to avoid dequeueFirst's allocation
+      // of Some instances:
+      if (cellsToExpandFrom.isEmpty) {
+        // no more steps/cells to try--no path exists
+        false
+      }
+      else {
+        val reachedAddr = cellsToExpandFrom.dequeue()
+        if (reachedAddr == toEmptyCell) {
+          // got to target--a path exists
+          true
+        }
+        else {
+          // no path yet; queue up neighbors neither ball-blocked nor already processed
+          neighborOffsets.foreach { (rowInc, colInc) =>
+            val rowOffset: Int = reachedAddr.row.raw.value    - Index.Origin + rowInc
+            val colOffset: Int = reachedAddr.column.raw.value - Index.Origin + colInc
+            if (! (   0 <= rowOffset && rowOffset < BoardOrder
+                   && 0 <= colOffset && colOffset < BoardOrder)) {
+              // off board--ignore this offset
+            } else if (blockedAt(rowOffset)(colOffset)) {
+              // ball-blocked or already traversed--ignore this offset
             }
-            loop
+            else {
+              // vacant--record traversed to and queue traversing from cell:
+              blockedAt(rowOffset).update(colOffset, true)
+              val neighborAddress = CellAddress(RowIndex.values(rowOffset),
+                                                ColumnIndex.values(colOffset))
+              cellsToExpandFrom.enqueue(neighborAddress)
+            }
           }
+          loopOnNextQueuedAddress
+        }
       }
     }
 
-    loop
+    val result = loopOnNextQueuedAddress
+    result
   }
 }
 
