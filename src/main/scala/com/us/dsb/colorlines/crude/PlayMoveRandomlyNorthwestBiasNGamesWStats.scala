7f1cee443d9998c0853429b1fbec3095695ad276
Daniel Barclay <daniel.barclay@qomplx.com>
2023-07-10 11:19:28 -0400

    Cleaned, clarified PlayMoveRandomlyNorthwestBiasNGamesWStats.

--- src/main/scala/com/us/dsb/colorlines/crude/PlayMoveRandomlyNorthwestBiasNGamesWStats.scala
+++ src/main/scala/com/us/dsb/colorlines/crude/PlayMoveRandomlyNorthwestBiasNGamesWStats.scala
@@ -1,7 +1,7 @@
 package com.us.dsb.colorlines.crude
 
 import com.us.dsb.colorlines.game.Parameters.BoardOrder
-import com.us.dsb.colorlines.game.board.{BoardReadView, CellAddress, Index}
+import com.us.dsb.colorlines.game.board.{BoardReadView, CellAddress, ColumnIndex, Index, RowIndex}
 
 import scala.util.Random
 
@@ -14,14 +14,18 @@ object PlayMoveRandomlyNorthwestBiasNGamesWStats extends App {
 
   val player: Player = new Player {
     override def chooseMove(board: BoardReadView): PlayerMove = {
-      // ????? TODO:  Use Index.Origin and BoardOrder, or use Index.MinValue and .MaxValue (or some range-size value)?
-      // bias:  9 gets higher probably (81 - 64 = 17 out of 81 than 1 (1 - 0 = 0))
-      val fromRow: Int = math.sqrt(rng.nextInt(BoardOrder * BoardOrder)).toInt + Index.Origin
-      val fromCol: Int = math.sqrt(rng.nextInt(BoardOrder * BoardOrder)).toInt + Index.Origin
-      val toRow: Int = math.sqrt((BoardOrder * BoardOrder - Index.Origin)
-                                     - rng.nextInt(BoardOrder * BoardOrder)).toInt + Index.Origin
-      val toCol: Int = math.sqrt((BoardOrder * BoardOrder - Index.Origin)
-                                     - rng.nextInt(BoardOrder * BoardOrder)).toInt + Index.Origin
+      // Bias via squares:  9 gets higher probabiliy (81 - 64 = 17, out of 81)
+      //   than 1 (1 - 0 = 1, out of 81)):
+      val rowCountSquared = RowIndex.values.size * RowIndex.values.size
+      val colCountSquared = ColumnIndex.values.size * ColumnIndex.values.size
+      val fromRow = math.sqrt(rng.nextInt(rowCountSquared)).toInt + Index.Origin
+      val fromCol = math.sqrt(rng.nextInt(colCountSquared)).toInt + Index.Origin
+      val toRow =
+        math.sqrt((rowCountSquared - Index.Origin) - rng.nextInt(rowCountSquared)).toInt
+            + Index.Origin
+      val toCol =
+        math.sqrt((colCountSquared - Index.Origin) - rng.nextInt(colCountSquared)).toInt
+            + Index.Origin
 
       val from = CellAddress.fromRaw(fromRow, fromCol)
       val to = CellAddress.fromRaw(toRow, toCol)
