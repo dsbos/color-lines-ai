3c61f5633ab4bc827028947cc5a13007c96eea91
Daniel Barclay <daniel.barclay@qomplx.com>
2023-07-22 12:06:22 -0400

    ....expl.nn:  half-adder:  Separated out ScalarTypes (opaque Double types).

--- src/main/scala/com/us/dsb/colorlines/expl/nn/ScalarTypes.scala
+++ src/main/scala/com/us/dsb/colorlines/expl/nn/ScalarTypes.scala
@@ -5,7 +5,7 @@ import scala.annotation.targetName
 // Note;  In separate scope from array code for better opaqueness in array-
 //   related code.
 /**
- * Scalar/numeric types.
+ * Common scalar/numeric types.
  */
 // ???? TODO:  Revisit name; "core"? "low-level"? (not nec'ly all scalar)
 object ScalarTypes {
@@ -14,15 +14,11 @@ object ScalarTypes {
   opaque type Weight     = Double
   opaque type Activation = Double
 
-  object Bias:
-    def apply(value: Double): Bias       = value
+  object Bias       { def apply(value: Double): Bias       = value }
+  object Weight     { def apply(value: Double): Weight     = value }
+  object Activation { def apply(value: Double): Activation = value }
 
-  object Weight:
-    def apply(value: Double): Weight     = value
-
-  object Activation:
-    def apply(value: Double): Activation = value
-
+  // (Note;  I wanted to avoid needing import Bias.* and siblings; hence @targetName:)
   extension (raw: Bias)       @targetName("Bias_raw")       def raw: Double = raw
   extension (raw: Weight)     @targetName("Weight_raw")     def raw: Double = raw
   extension (raw: Activation) @targetName("Activation_raw") def raw: Double = raw
