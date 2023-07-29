5c6a9021fc7c08816b8843e037b0904e15747fd9
Daniel Barclay <daniel.barclay@qomplx.com>
2023-07-29 19:51:37 -0400

    ....expl.nn2...:  ~Extracted NeuralNetworkSimpleImpl from AssimilateTheseNNModelTypes sketch.

--- src/main/scala/com/us/dsb/colorlines/expl/nn/AssimilateTheseNNModelTypes.scala
+++ src/main/scala/com/us/dsb/colorlines/expl/nn/AssimilateTheseNNModelTypes.scala
@@ -3,10 +3,7 @@ package com.us.dsb.colorlines.expl.nn
 import com.us.dsb.colorlines.expl.nn2.ActivationFunctions
 import com.us.dsb.colorlines.expl.nn2.types.LowlevelTypes.{
   Activation, Bias, LayerActivations, Weight, raw}
-import com.us.dsb.colorlines.expl.nn2.types.NeuralNetworkSimpleImpl.{
-  Layer, Network, Neuron}
 
-import scala.collection.immutable.ArraySeq
 
 object AssimilateTheseNNModelTypes extends App {
 
@@ -42,70 +39,5 @@ object AssimilateTheseNNModelTypes extends App {
       val rawAct = ActivationFunctions.standardLogisticFunction(sumAccum + neuronBias.raw)
       Activation(rawAct)
     }
-
-//    val nn1: Model1.NeuralNetworkConfig = ???
-//    val nn2: Model2.NeuralNetworkConfig = ???
-//    val nn3: Model3.NeuralNetworkConfig = ???
-//    val nn4: Model4.NeuralNetworkConfig = ???
-//    val nn5: Model5.NeuralNetworkConfig = ???
-
-    object Model5Impl {
-
-    }
-
-    val nn5 = {
-      val inputCount = 2
-      new Network(
-        inputCount,
-        Vector(
-          Layer(inputCount,
-                Vector(
-                  Neuron(inputCount,
-                         Bias(2.1),
-                         Vector(Weight(23.101), Weight(23.102))),
-                  Neuron(inputCount,
-                         Bias(2.2),
-                         Vector(Weight(23.201), Weight(23.202))),
-                  Neuron(inputCount,
-                         Bias(2.3),
-                         Vector(Weight(23.301), Weight(23.302)))
-                )),
-          Layer(3,
-                Vector(
-                  Neuron(3,
-                         Bias(2.4),
-                         Vector(Weight(23.401), Weight(23.402), Weight(23.403)))
-                ))
-          )
-        )
-    }
-
-    def makeInputActivations(count: Int): LayerActivations =
-      LayerActivations(ArraySeq.fill(count)(Activation(1.23)))
-
-
-
-
-    val outputActivations5: LayerActivations = {
-      val inputActivations = makeInputActivations(nn5.inputCount)
-      nn5.layers.foldLeft(inputActivations) { (inActs, layer) =>
-        val outActs =
-          layer.neurons.map { neuronData =>
-            computeNeuronActivationViaSeqs(
-              inputActivations   = inActs,
-              neuronInputWeights = neuronData.weights,
-              neuronBias         = neuronData.bias)
-            // (OR:)
-            computeNeuronActivationViaFns(
-              inputSize          = inActs.vector.size,
-              inputActivations   = inActs.vector,
-              neuronInputWeights = neuronData.weights,
-              neuronBias         = neuronData.bias)
-          }
-        LayerActivations(outActs)
-      }
-    }
-
   }
-  Temp
 }
