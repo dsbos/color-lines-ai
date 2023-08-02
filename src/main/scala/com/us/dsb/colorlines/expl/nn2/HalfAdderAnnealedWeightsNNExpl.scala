d0e31cc4ac3b11fb9f31588f6b07e6e0ee5a5749
Daniel Barclay <daniel.barclay@qomplx.com>
2023-08-02 13:23:45 -0400

    ....expl.nn2...:  Factored out much more into HalfAddedCommon.

--- src/main/scala/com/us/dsb/colorlines/expl/nn2/HalfAdderAnnealedWeightsNNExpl.scala
+++ src/main/scala/com/us/dsb/colorlines/expl/nn2/HalfAdderAnnealedWeightsNNExpl.scala
@@ -2,7 +2,6 @@ package com.us.dsb.colorlines.expl.nn2
 
 import com.us.dsb.colorlines.expl.nn2.ActivationComputation.{
   ActivationFunction, computeNetworkOutputActivation}
-import com.us.dsb.colorlines.expl.nn2.ActivationFunctions
 import com.us.dsb.colorlines.expl.nn2.types.LowlevelTypes.{
   Activation, Bias, LayerActivations, Weight, raw}
 import com.us.dsb.colorlines.expl.nn2.types.NeuralNetworkSimpleImpl.{
@@ -18,7 +17,7 @@ object HalfAdderAnnealedWeightsNNExpl extends App {
       val `rand_0_to_1`: Double = Random.nextFloat()
       //?????? parameterize
       val `rand_-1_to_1`: Double = (`rand_0_to_1` - 0.5) * 2
-      val `rand_-x_to_x`: Double = `rand_-1_to_1` * 10 // 20 // 5 // 20 //????
+      val `rand_-x_to_x`: Double = `rand_-1_to_1` * 20 // 5 // 20 //????
       //println(s"randomSomething: ${`rand_-x_to_x`}")
       `rand_-x_to_x`
     }
@@ -70,65 +69,11 @@ object HalfAdderAnnealedWeightsNNExpl extends App {
   private val activationFunction: ActivationFunction =
     raw => Activation(ActivationFunctions.standardLogisticFunction(raw))  //??????
 
-  //?????? rename; longer; "evaluate" sounds like evaluating fitness, vs. evaluating
-  //  next function; ** "execute"? "compute"? ~~"evaluate function"
-  //?????? Maybe wrap Byte in Bit opaque type to limit to 0 and 1:
-  //?????? Possibly wrap Double in something reflecting approximated "bitness":
-  def eval(nw: NetworkConfig,
-           inputs: (Byte, Byte, Byte)
-          ): (Double, Double) = {
-
-    // ???? TODO:  Is there way to map tuple to collection?  Or have fixed length
-    // on collection?  (Tuple inputs and outputs were for fixed lengths for half-adder.)
-    // ???? TODO:  Should LayerActivations have var-args ~constructor?
-    val inputActivations = LayerActivations(IndexedSeq(Activation(inputs._1),
-                                                       Activation(inputs._2),
-                                                       Activation(inputs._3)))  //??????
-    val outputActivations  =
-      computeNetworkOutputActivation(nw, activationFunction, inputActivations)
-    (outputActivations.vector(0).raw,
-        outputActivations.vector(1).raw)
-  }
-
-  // ?? TODO: _Possibly_ wrap Double in some opaque fitness type:
-  def computeFitness(nw: NetworkConfig): Double =
-    HalfAdderCommon.computeFitness((a1: Byte, a2: Byte, a3: Byte) => eval(nw, (a1, a2, a3)))
-
   def makeHalfAdderNetwork: NetworkConfig = makeBaseNetwork(3, 5, 2)
 
   def makeDerivedHalfAdderNetwork(base: NetworkConfig): NetworkConfig =
     randomlyDeriveNetwork(base)
 
-
-  val startMs = System.currentTimeMillis()
-  var curr = makeHalfAdderNetwork
-  var currFitness = computeFitness(curr)
-  val maxIterations = 10_000_000
-  var iterations = 0
-  while iterations < maxIterations do {
-    iterations += 1
-    if (1 == iterations % 1_000_000) {
-      val pct = 100.0 * iterations / maxIterations
-      println(f"@ $iterations (/$maxIterations, ${pct}%4.1f%%) currFitness = $currFitness%11.5g ...")
-    }
-    val cand = makeDerivedHalfAdderNetwork(curr)
-    val candFitness = computeFitness(cand)
-
-    if candFitness > currFitness then {
-      println(f"@ $iterations: base: $currFitness%11.5g -> cand: $candFitness%11.5g")
-      HalfAdderCommon.cases.foreach { case ((a1, a2, a3), (c, s)) =>
-        val nnOutput = eval(cand, (a1, a2, a3))
-        if ! true then
-          println(f"$a1 + $a2 + $a3 = $c $s: ${nnOutput._1}%11.5g, ${nnOutput._2}%11.5g"
-                      + f";  ∆c = ${nnOutput._1 - c}%11.5g"
-                      + f", ∆s = ${nnOutput._2 - s}%11.5g")
-      }
-      curr = cand
-      currFitness = candFitness
-    }
-  }
-  val endMs = System.currentTimeMillis()
-  val durationMs = endMs - startMs
-  println(s"for $iterations iterations, durationMs = $durationMs ms"
-              + s" (${durationMs * 1.0 / iterations} ms/iteration)")
+  HalfAdderCommon.run(makeHalfAdderNetwork,
+                      base => makeDerivedHalfAdderNetwork(base))
 }
--- src/main/scala/com/us/dsb/colorlines/expl/nn2/HalfAdderCommon.scala
+++ src/main/scala/com/us/dsb/colorlines/expl/nn2/HalfAdderCommon.scala
@@ -1,5 +1,10 @@
 package com.us.dsb.colorlines.expl.nn2
 
+import com.us.dsb.colorlines.expl.nn2.ActivationComputation.{
+  ActivationFunction, computeNetworkOutputActivation}
+import com.us.dsb.colorlines.expl.nn2.types.LowlevelTypes.{Activation, LayerActivations}
+import com.us.dsb.colorlines.expl.nn2.types.NeuralNetworkReadView.NetworkConfig
+
 object HalfAdderCommon {
 
   /**
@@ -22,11 +27,18 @@ object HalfAdderCommon {
         }
   }
 
+  /**
+   * Abstract execution function for half adder:  Input bit addends to approximate 2-bit output.
+   */
   type ExecutionFunction = (Byte, Byte, Byte) => (Double, Double)
   //?????? Revisit names:
   // - case execution function
   // - network evaluation/fitness function
-  def computeFitness(executeNetwork: ExecutionFunction): Double = {
+
+  /**
+   * Fitness function for network for half adder, give abstract execution function.
+   */
+  def computeFunctionFitness(executeNetwork: ExecutionFunction): Double = {
     val fitness =
       cases.map { case ((a1, a2, a3), (c, s)) =>
         val nnOutput = executeNetwork(a1, a2, a3)
@@ -38,6 +50,70 @@ object HalfAdderCommon {
       }.sum
     //println(s"fitness = $fitness")
     fitness
+  }
+
+  private val activationFunction: ActivationFunction =
+    raw => Activation(ActivationFunctions.standardLogisticFunction(raw))
+
+  //?????? Maybe wrap Byte in Bit opaque type to limit to 0 and 1:
+  //?????? Possibly wrap Double in something reflecting approximated "bitness":
+  def executeNetwork(nw: NetworkConfig,
+                     inputs: (Byte, Byte, Byte)
+                    ): (Double, Double) = {
+
+    // ???? TODO:  Is there way to map tuple to collection?  Or have fixed length on
+    //   collection?  (Tuple inputs and outputs are for fixed lengths for half-adder.)
+    // ???? TODO:  Should LayerActivations have var-args ~constructor?
+    val inputActivations = LayerActivations(IndexedSeq(Activation(inputs._1),
+                                                       Activation(inputs._2),
+                                                       Activation(inputs._3)))
+    val outputActivations =
+      computeNetworkOutputActivation(nw, activationFunction, inputActivations)
+    (outputActivations.vector(0).raw,
+        outputActivations.vector(1).raw)
+  }
+
+  // ?? TODO: _Possibly_ wrap Double in some opaque fitness type:
+  /**
+   * Fitness function for network for half adder, give neural network function.
+   */
+  def computeNetworkFitness(nw: NetworkConfig): Double =
+    HalfAdderCommon.computeFunctionFitness(
+      (a1: Byte, a2: Byte, a3: Byte) => executeNetwork(nw, (a1, a2, a3)))
+
+
+  def run(makeInitialNetwork: NetworkConfig,
+          makeCandidateNetwork: NetworkConfig => NetworkConfig): Unit = {
+    val startMs = System.currentTimeMillis()
+    var curr = makeInitialNetwork
+    var currFitness = computeNetworkFitness(curr)
+    val maxIterations = 100_000_000
+    var iterations = 0
+    while iterations < maxIterations do {
+      iterations += 1
+      if (1 == iterations % 1_000_000) {
+        val pct = 100.0 * iterations / maxIterations
+        println(f"@ $iterations (/$maxIterations, ${pct}%4.1f%%) currFitness = $currFitness%8.5f ...")
+      }
+      val cand = makeCandidateNetwork(curr)
+      val candFitness = computeNetworkFitness(cand)
+
+      if candFitness > currFitness then {
+        println(f"@ $iterations: base: $currFitness%8.5f -> cand: $candFitness%8.5f")
+        HalfAdderCommon.cases.foreach { case ((a1, a2, a3), (c, s)) =>
+          val nnOutput = executeNetwork(cand, (a1, a2, a3))
+          println(f"$a1 + $a2 + $a3 = $c $s: ${nnOutput._1}%8.5f, ${nnOutput._2}%8.5f"
+                      + f";  ∆c = ${nnOutput._1 - c}%8.5f"
+                      + f", ∆s = ${nnOutput._2 - s}%8.5f")
+        }
+        curr = cand
+        currFitness = candFitness
+      }
+    }
+    val endMs = System.currentTimeMillis()
+    val durationMs = endMs - startMs
+    println(s"for $iterations iterations, durationMs = $durationMs ms"
+                + s" (${durationMs * 1.0 / iterations} ms/iteration)")
   }
 
 }
